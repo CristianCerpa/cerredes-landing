8738a85e7123f81c28053520d0f5c00828b33998
CristianCerpa <ccerpaj@gmail.com>
2025-09-04 12:15:30 -0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{minHeight:'100vh', background:'#000', color:'#fff', display:'flex', alignItems:'center', justifyContent:'center'}}>
-      <h1>Hola CERREDES 🚀</h1>
-    </main>
-  );
-}
