a434d20d4cb067d318241e487c2bbdaff054c3b1
CristianCerpa <ccerpaj@gmail.com>
2025-09-04 11:40:38 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{minHeight:'100vh', background:'#000', color:'#fff', display:'flex', alignItems:'center', justifyContent:'center'}}>
+      <h1>Hola CERREDES 🚀</h1>
+    </main>
+  );
+}
